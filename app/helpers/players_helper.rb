4de275c82920305daa45ddf116b0f82b16fa5b91
Jakub <qoobaa@gmail.com>
2009-06-07 22:52:19 +0200

    deleted unneeded scripts and helpers

--- app/helpers/players_helper.rb
+++
@@ -1,8 +0,0 @@
-module PlayersHelper
-  def classes_for_player(player)
-    classes = []
-    classes << "player-#{player.number}"
-    classes << "current" if player.current?
-    classes.join(" ")
-  end
-end
